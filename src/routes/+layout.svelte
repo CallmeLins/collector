8aa8acfd900a6587e70ca721225b92fe7a65116c
Hao <zhao5638@gmail.com>
2026-04-18 20:40:53 +0800

    feat(ui): add system theme mode to header theme switcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,5 @@
 <script>
 	import '../app.css';
-	import { onMount } from 'svelte';
-	import { themeChange } from 'theme-change';
-
-	onMount(() => {
-		themeChange(false);
-		// 👆 false parameter is required for svelte
-	});
 
 	let { children } = $props();
 </script>
